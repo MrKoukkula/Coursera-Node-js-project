f67ef18dbe30934917e6cdff408a2f7ff05066c9
MrKoukkula <pe.koukkula@gmail.com>
2020-08-10 23:00:59 +0300

    mongoose added

--- mongoTest.js
+++ mongoTest.js
@@ -1,33 +1,72 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
 const dboper = require('./operations');
+const Dishes = require('./models/dish');
+const mongoose = require('mongoose');
 
-const url = "mongodb://localhost:27017";
-const dbname = 'conFusion';
+const url = "mongodb://localhost:27017/conFusion";
+// const dbname = 'conFusion';
+const connect = mongoose.connect(url);
 
-MongoClient.connect(url, (err, client) => {
+connect.then((db) => {
+    console.log('Connected to the server');
 
-    if (err) {
-        return new Error('failed to connect');
-    }
+    Dishes.create({
+        name: 'testypizza',
+        description: 'very tasty'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated tastiness'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.find();
+    }).then((dishes) => {
+        console.log('found dishes:')
+        console.log(dishes);
+
+        return Dishes.remove();
+    }).then(() => {
+        return mongoose.connection.close()
+    }).catch((err) => console.log(err))
+})
+
+/* MongoClient.connect(url, (err, client) => {
+    if (err) return new Error('failed to connect');
 
     console.log("Connected to the server");
 
     const db = client.db(dbname);
     
-    dboper.insertDocument(db, {name: "pizza", description: "Tasty pizza"}, "dishes", (result) => {
-        console.log('inserted file');
-
-        dboper.findDocuments(db, 'dishes', (docs) => {
-            console.log(docs);
-
-            db.dropCollection('dishes', (result) => {
-                console.log('shut down');
-
-                client.close();
-            })
-        })
+    dboper.insertDocument(db, {name: "pizza", description: "Tasty pizza"}, "dishes")
+    .then( result => {
+        return dboper.findDocuments(db, 'dishes')
     })
-
-    
-
-})
+    .then(result => {
+        console.log(result);
+        return db.dropCollection('dishes')
+    })
+    .then(result => {
+        console.log('dropped collection');
+        client.close();
+    })
+    .catch(err => console.log(err))
+}) */
--- operations.js
+++ operations.js
@@ -1,34 +1,19 @@
 exports.insertDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.insert(document, (err, result) => {
-        if (err) return new Error('inserting failed');
-        console.log('Inserted document');
-        callback(result);
-    })
+    return coll.insertOne(document);
 }
 
 exports.findDocuments = (db, collection, callback) => {
     const coll = db.collection(collection);
-    coll.find().toArray((err, docs) => {
-        if (err) return new Error('finding failed');
-        callback(docs);
-    })
+    return coll.find().toArray()
 }
 
 exports.removeDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        if (err) return new Error('deleting failed');
-        console.log('deleted document');
-        callback(result);
-    })
+    return coll.deleteOne(document)
 }
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        if (err) return new Error('updating failed');
-        console.log('updated document');
-        callback(result);
-    })
+    return coll.updateOne(document, { $set: update }, null)
 }
