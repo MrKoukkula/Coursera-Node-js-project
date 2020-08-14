d2d9ba5357ccd0f4941ca956bef3d901ee57c52f
MrKoukkula <pe.koukkula@gmail.com>
2020-08-14 14:20:17 +0300

    Added verifyadmin function

--- authenticate.js
+++ authenticate.js
@@ -37,3 +37,12 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+    if (!req.user.admin) {
+        err = new Error('You are not authorized to post dishes');
+        err.status = 404;
+        return next(err);
+      }
+    next();
+}
